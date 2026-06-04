3fc32530775e085cba34392cc9474c01a364e97f
joelyakubu269 <joelyakubu269@gmail.com>
2026-06-05 00:30:30 +0100

    writing timer function

--- Timer/js/timer.js
+++ Timer/js/timer.js
@@ -1,16 +1,28 @@
 let secondsElapsed = 0
 let intervaL = null
+const time= document.getElementById("id")
+
 const padTime=(value)=>  String(value).padStart(2,"0")
 const setTime=()=>{
- const minutes = Math.floor(seconds/60)
-    const remainingMinutes= seconds % 60
+ const minutes = Math.floor(secondsElapsed/60)
+    const remainingMinutes= secondsElapsed % 60
     return `${padTime(minutes)}: ${padTime(remainingMinutes)}` 
 }
 const StartTimer=()=> {
    secondsElapsed++
    setTime()
+   
 }
 const start=()=>{
     intervaL= setInterval(StartTimer,1000)
-    
+    setTime()
+    time.textContent= setTime()
+}
+const stop=()=>{
+     clearInterval(intervaL)
+    setTime()
+}
+const Reset=()=> {
+    clearInterval(intervaL)
+    setTime()
 }
