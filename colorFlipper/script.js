d846de3644028a5e71113445dd8da1e42a3f2c63
joelyakubu269 <joelyakubu269@gmail.com>
2026-06-04 08:26:58 +0100

    trying to make a function for random button

--- colorFlipper/script.js
+++ colorFlipper/script.js
@@ -2,8 +2,10 @@ const body = document.getElementsByTagName("body")[0]
 const change=(name)=> {
     body.style.backgroundColor= name
 }
-// const random=()=>{
-//     const colArray= ["red","blue","green","yellow","black","white","grey"] 
-//     const buffer= [...colArray]
-//     const randomIdx = 
-// }
+const random=()=>{
+    const colArray= ["red","blue","green","yellow","black","white","grey"] 
+    const buffer= [...colArray]
+    const randomIdx = Math.floor(Math.random() * buffer.length)
+    const name = buffer[randomIdx]
+    buffer.slice(randomIdx,1)
+}
